f0129641027a376eaaa06bdf7df9bf31d59752d5
paras <parasjuly2003@gmail.com>
2024-11-09 10:32:14 +0530

    Upto Page 5

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// using gsap library for animation 
+// using gsap library for animation
 //for revision read the documentation for gsap
 function navAnimation() {
   var nav = document.querySelector("nav");
 
   nav.addEventListener("mouseenter", function () {
-    let tl = gsap.timeline();//synchronize the code line by line
+    let tl = gsap.timeline(); //synchronize the code line by line
 
     tl.to("#nav-bottom", {
       height: "22vh",
@@ -41,3 +41,71 @@ function navAnimation() {
 }
 
 navAnimation();
+
+function page2Animation() {
+  var rightElems = document.querySelectorAll(".right-elem");
+
+  rightElems.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      // console.log(elem.childNodes); // try this
+      gsap.to(elem.childNodes[3], {
+        //we are doing 3rd index cause image is there is 3rd index
+        opacity: 1,
+        scale: 1,
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to(elem.childNodes[3], {
+        opacity: 0,
+        scale: 0,
+      });
+    });
+    elem.addEventListener("mousemove", function (dets) {
+      // console.log(dets); // details on mouse events
+      gsap.to(elem.childNodes[3], {
+        x: dets.x - elem.getBoundingClientRect().x - 90, //with getBoundingClientRect we get the exact position of the pointer
+        y: dets.y - elem.getBoundingClientRect().y - 215,
+      });
+    });
+  });
+}
+
+page2Animation();
+
+function page3VideoAnimation() {
+  var page3Center = document.querySelector(".page3-center");
+  var video = document.querySelector("#page3 video");
+
+  page3Center.addEventListener("click", function () {
+    video.play();
+    gsap.to(video, {
+      transform: "scaleX(1) scaleY(1)",//full scale - screen
+      opacity: 1,
+      borderRadius: 0,
+    });
+  });
+  video.addEventListener("click", function () {
+    video.load();//runs again instead of play from last run
+    gsap.to(video, {
+      transform: "scaleX(0.7) scaleY(0)",
+      opacity: 0,
+      borderRadius: "30px",
+    });
+  });
+  
+  //page 4
+  var sections = document.querySelectorAll(".sec-right");
+  
+  sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      elem.childNodes[3].style.opacity = 1;
+      elem.childNodes[3].play();
+    });
+    elem.addEventListener("mouseleave", function () {
+      elem.childNodes[3].style.opacity = 0;
+      elem.childNodes[3].load();
+    });
+  });
+}
+
+page3VideoAnimation();
